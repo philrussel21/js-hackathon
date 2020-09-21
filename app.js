1ff089f4e95a2f001e62f58934ada6670dd07932
philrussel21 <phil.antiporda21@gmail.com>
2020-09-21 14:32:44 +0930

    structures the code to a more functional programming

--- app.js
+++ app.js
@@ -1,32 +1,12 @@
 
 // name goes here: would be coming from the player input
-const playerFirstName = 'lebron'
-const playerLastName = 'james'
-axios.get(`https://www.balldontlie.io/api/v1/players?search=${playerFirstName}+${playerLastName}`)
-  .then(({ data }) => {
-    const { data: player } = data
-    // if results has more than one record, create a dynamic dropdown menu consisting all of the records to
-    // further filter the results
-    if (player.length > 1) {
-      console.log('more than one player')
-    }
-    // only one record
-    else {
-      // logs an array of player data
-      console.log('only one player')
+const playerInput = 'lebron '
+const playerName = playerInput.trim().split(' ')
+// conditional assignment to variable.
+const playerQuery = (playerName.length === 1) ? playerName[0] : `${playerName[0]}+${playerName[1]}`
 
-      // destructures the array and assigns to variable
-      const [playerData] = player
-      const playerId = playerData.id
-      const playerName = `${playerData.first_name} ${playerData.last_name}`
-      // destructures and assigns the full name of the player's team to the var
-      const { full_name: playerTeam } = playerData.team
-      console.log({ playerId, playerName, playerTeam })
-      showPlayerStats(playerId)
-    }
-  }).catch((err) => {
-    console.log(err)
-  });
+// MAIN FLOW OF THE PROGRAM - to be changed when DOM is set up
+getPlayerInfo(playerQuery)
 
 // queries a certain player according to id and prints their stats accordingly
 // TODO - stats to print in DOM
@@ -45,4 +25,38 @@ async function showPlayerStats(id) {
   const statSteals = playerStats.stl
   const statMinutes = playerStats.min
   console.log({ statAssists, statBlocks, statMinutes, statPoints, statRebounds, statSeason, statSteals })
+  // TODO - playerStats written on DOM
+}
+
+function showPlayerInfo(data) {
+  const playerId = data.id
+  const playerFullName = `${data.first_name} ${data.last_name}`
+  // destructures and assigns the full name of the player's team to the var
+  const { full_name: playerTeam } = data.team
+  console.log({ playerId, playerFullName, playerTeam })
+  // TODO - playerInfo written on DOM
+  showPlayerStats(playerId)
+}
+
+function getPlayerInfo(query) {
+  axios.get(`https://www.balldontlie.io/api/v1/players?search=${playerQuery}`)
+    .then(({ data }) => {
+      const { data: player } = data
+      // if results has more than one record, create a dynamic dropdown menu consisting all of the records to
+      // further filter the results
+      if (player.length > 1) {
+        console.log('more than one player')
+      }
+      // only one record
+      else {
+        // logs an array of player data
+        console.log('only one player')
+
+        // destructures the array and assigns to variable
+        const [playerData] = player
+        showPlayerInfo(playerData)
+      }
+    }).catch((err) => {
+      console.log(err)
+    });
 }
