f0b8de244c5c4b1c3ffd28d7c47635418bb32f22
philrussel21 <phil.antiporda21@gmail.com>
2020-09-22 15:00:45 +0930

    Adds catch statements for invalid inputs and unexpected errors

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const teamsDropdown = document.querySelector('#nbaTeams')
 const gamesDropdown = document.querySelector('#recentGames')
 const qSubmitBtn = document.querySelector('#qBtn')
 const qDiv = document.querySelector('#showcase')
+const qDivHeading = document.createElement('h3')
 
 // should run onload to fill dropdown menu
 window.onload = () => {
@@ -22,7 +23,12 @@ qSubmitBtn.addEventListener('click', (e) => {
 
   if (playerInput.value) {
     const playerName = playerInput.value
-    getPlayerInfo(playerName)
+    try {
+      getPlayerInfo(playerName)
+
+    } catch (error) {
+      console.log(error)
+    }
 
     playerInput.value = ""
   }
@@ -46,7 +52,7 @@ playerInput.addEventListener('blur', function () {
 
 // TODO - accomodate to when no team is selected and league recent games is shown
 // BUG - once user selects a team, cannot change mind to query a player instead
-teamsDropdown.addEventListener('change', function () {
+teamsDropdown.addEventListener('input', function () {
   playerInput.disabled = true
 
   const teamId = this.value
@@ -54,47 +60,8 @@ teamsDropdown.addEventListener('change', function () {
   getRecentGames(teamId)
 })
 
-
-// id would come from user input from dropdown menu
-// getRecentGames(2)
-
-// id would come from user input from recent games dropdown menu
-// getTopPerformers(123540)
-
-
-// // name goes here: would be coming from the player input
-// const playerInput = 'andre'
-
-
-// MAIN FLOW OF THE PROGRAM - to be changed when DOM is set up
-// getPlayerInfo(playerInput)
-
-// queries a certain player according to id and prints their stats accordingly
-// TODO - stats to print in DOM
-async function getPlayerStats(id) {
-  // query can accept another parameter for season. defaults to current season as below.
-  try {
-    const response = await axios.get(`https://www.balldontlie.io/api/v1/season_averages?player_ids[]=${id}`)
-    // stores an object that has the player's average on the given season
-    const [playerStats] = response.data.data
-
-    showPlayerStats(playerStats)
-    // const stats = { statAssists, statBlocks, statMinutes, statPoints, statRebounds, statSeason, statSteals }
-    // TODO - playerStats written on DOM
-  } catch (error) {
-    console.log('Unavailable Data for selected player')
-  }
-
-}
-
+// FUNCTIONS
 function showPlayerStats(data) {
-  // const statSeason = data.season
-  // const statPoints = data.pts
-  // const statAssists = data.ast
-  // const statRebounds = data.reb
-  // const statBlocks = data.blk
-  // const statSteals = data.stl
-  // const statMinutes = data.min
   const { season, pts, ast, reb, blk, stl, min } = data
   const categories = ['points', 'assists', 'rebounds', 'blocks', 'steals', 'minutes']
   const stats = [pts, ast, reb, blk, stl, min]
@@ -131,42 +98,11 @@ function showPlayerInfo(data) {
   playerH3.textContent = playerFullName
   playerP.textContent = playerTeam
   playerDiv.append(playerH3, playerP)
+
+  clearqDiv()
   qDiv.append(playerDiv)
 
   getPlayerStats(playerId)
-}
-
-function getPlayerInfo(input) {
-  const playerName = input.trim().split(' ')
-  // conditional assignment to variable.
-  const playerQuery = (playerName.length === 1) ? playerName[0] : `${playerName[0]}+${playerName[1]}`
-  axios.get(`https://www.balldontlie.io/api/v1/players?per_page=50&search=${playerQuery}`)
-    .then(({ data }) => {
-      const { data: player } = data
-      // if results has more than one record, create a dynamic dropdown menu consisting all of the records to
-      // further filter the results
-      if (player.length > 1) {
-        const matchingPlayersDropdown = showMultiplePlayers(player)
-        matchingPlayersDropdown.addEventListener('change', function () {
-          const chosenPlayer = this.value
-          getPlayerInfo(chosenPlayer)
-        })
-        // TODO - matchingNames to be shown to user as dropdown menu, then the selected playerName
-        // would be passed as the argument in the function below.
-        // FUTURE - can be refactored to save one API call by using showPlayerInfo() and adding the id and team name with the playerName
-        // getPlayerInfo(matchingNames[0])
-
-      }
-      // only one record
-      else {
-
-        // destructures the array and assigns to variable
-        const [playerData] = player
-        showPlayerInfo(playerData)
-      }
-    }).catch((err) => {
-      console.log(err)
-    });
 }
 
 function showMultiplePlayers(arrOfPlayers) {
@@ -182,15 +118,8 @@ function showMultiplePlayers(arrOfPlayers) {
     optn.textContent = `${player.first_name} ${player.last_name}`
     playersDropdown.append(optn)
   }
-  qDiv.append(playersDropdown)
+  form.append(playersDropdown)
   return playersDropdown
-}
-
-async function getAllTeams() {
-  const response = await axios.get("https://www.balldontlie.io/api/v1/teams")
-  // unpacks response Array data to variable teams
-  const { data: { data: teams } } = response
-  showAllTeams(teams)
 }
 
 function showAllTeams(arr) {
@@ -207,21 +136,6 @@ function showAllTeams(arr) {
 
 }
 
-// id would come from user input from team dropdown menu
-async function getRecentGames(id) {
-  // FUTURE - can also be dynamic
-  const season = 2019
-  // grabs today's date object then converts to specified format to be used for query
-  const userDate = setUserDate()
-  const response = await axios.get(`https://www.balldontlie.io/api/v1/games?seasons[]=${season}&team_ids[]=${id}&per_page=100&end_date=${userDate}`)
-  // unpacks response Array data to variable seasonGames
-  const { data: { data: seasonGames } } = response
-  // extracts the last 10 recent games of the selected team
-  const teamRecentGames = seasonGames.slice(-10).reverse()
-  showRecentGames(teamRecentGames)
-
-}
-
 function showRecentGames(arr) {
 
   // sorts the game in descending order to show most recent first
@@ -233,6 +147,7 @@ function showRecentGames(arr) {
     return aa < bb ? 1 : (aa > bb ? -1 : 0);
   });
 
+  gamesDropdown.innerHTML = ""
   for (let game of arr) {
     // only grabs YYYY-MM-DD from the date string
     const gameId = game.id
@@ -266,29 +181,6 @@ function setUserDate() {
   return [year, month, day].join('-');
 }
 
-// id would come from user input from recent games dropdown menu
-async function getTopPerformers(id) {
-  const teamsRequest = axios.get(`https://www.balldontlie.io/api/v1/games/${id}`)
-  const statsRequest = axios.get(`https://www.balldontlie.io/api/v1/stats?game_ids[]=${id}&per_page=50`)
-  const response = await Promise.all([teamsRequest, statsRequest])
-  const [teamsResponse, statsResponse] = response
-
-  const homeTeamId = teamsResponse.data.home_team.id
-
-  // unpacks statsResponse Array data to variable gameStats
-  const { data: { data: gameStats } } = statsResponse
-
-  // assigns players to associated teams
-  const homeTeamPlayers = []
-  const awayTeamPlayers = []
-  for (let player of gameStats) {
-    player.team.id === homeTeamId ? homeTeamPlayers.push(player) : awayTeamPlayers.push(player)
-  }
-
-  showTopPerformers(homeTeamPlayers)
-  showTopPerformers(awayTeamPlayers)
-}
-
 function showTopPerformers(team) {
   const topPerformers = {}
   const topPoints = team.reduce((top, player) => player.pts > top.pts ? player : top, team[0])
@@ -296,8 +188,8 @@ function showTopPerformers(team) {
   const topRebounds = team.reduce((top, player) => player.reb > top.reb ? player : top, team[0])
   const topBlocks = team.reduce((top, player) => player.blk > top.blk ? player : top, team[0])
   const topSteals = team.reduce((top, player) => player.stl > top.stl ? player : top, team[0])
-  console.log({ topPoints, topAssists, topRebounds, topBlocks, topSteals })
-  // TODO - Show to DOM on the space provided
+
+  //Show to DOM on the space provided
   const div = document.createElement('div')
   const teamH3 = document.createElement('h3')
 
@@ -308,6 +200,7 @@ function showTopPerformers(team) {
   writeCategory(div, topRebounds, "rebounds", 'reb')
   writeCategory(div, topBlocks, "blocks", 'blk')
   writeCategory(div, topSteals, "steals", 'stl')
+
   qDiv.append(div)
 }
 
@@ -321,6 +214,134 @@ function writeCategory(cont, player, cat, attr) {
   cont.append(ul, li)
 }
 
+function clearqDiv() {
+  qDiv.innerHTML = ""
+}
+
+function errorMessage(msg) {
+  qDivHeading.textContent = msg
+  qDiv.append(qDivHeading)
+}
+
+// ASYNC FUNCTIONS - PROMISE
+// queries a certain player according to id and prints their stats accordingly
+async function getPlayerStats(id) {
+  // query can accept another parameter for season. defaults to current season as below.
+  try {
+    const response = await axios.get(`https://www.balldontlie.io/api/v1/season_averages?player_ids[]=${id}`)
+    // stores an object that has the player's average on the given season
+    const [playerStats] = response.data.data
+
+    showPlayerStats(playerStats)
+    // const stats = { statAssists, statBlocks, statMinutes, statPoints, statRebounds, statSeason, statSteals }
+    // TODO - playerStats written on DOM
+  } catch (error) {
+    console.log(error)
+    errorMessage("Unavailable Stats for Selected Player")
+  }
+
+}
+
+function getPlayerInfo(input) {
+  const playerName = input.trim().split(' ')
+  // conditional assignment to variable.
+  const playerQuery = (playerName.length === 1) ? playerName[0] : `${playerName[0]}+${playerName[1]}`
+  axios.get(`https://www.balldontlie.io/api/v1/players?per_page=50&search=${playerQuery}`)
+    .then(({ data }) => {
+      const { data: player } = data
+      // if results has more than one record, create a dynamic dropdown menu consisting all of the records to
+      // further filter the results
+      if (player.length > 1) {
+        // disableEls([playerInput, teamsDropdown])
+        playerInput.disabled = true
+        teamsDropdown.disabled = true
+        const matchingPlayersDropdown = showMultiplePlayers(player)
+        matchingPlayersDropdown.addEventListener('change', function () {
+          const chosenPlayer = this.value
+          getPlayerInfo(chosenPlayer)
+        })
+        // FUTURE - can be refactored to save one API call by using showPlayerInfo() and adding the id and team name with the playerName
+        // getPlayerInfo(matchingNames[0])
+      }
+      // only one record
+      else {
+
+        // destructures the array and assigns to variable
+        const [playerData] = player
+        showPlayerInfo(playerData)
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+      errorMessage("Unavailable Info for Player Wanted")
+    })
+}
+
+async function getAllTeams() {
+  try {
+    const response = await axios.get("https://www.balldontlie.io/api/v1/teams")
+    // unpacks response Array data to variable teams
+    const { data: { data: teams } } = response
+    showAllTeams(teams)
+  } catch (error) {
+    console.log(error)
+    errorMessage("Unavailable Info to show All NBA Teams")
+  }
+
+}
+
+// id would come from user input from team dropdown menu
+async function getRecentGames(id) {
+  try {
+    // FUTURE - can also be dynamic
+    const season = 2019
+    // grabs today's date object then converts to specified format to be used for query
+    const userDate = setUserDate()
+    const response = await axios.get(`https://www.balldontlie.io/api/v1/games?seasons[]=${season}&team_ids[]=${id}&per_page=100&end_date=${userDate}`)
+    // unpacks response Array data to variable seasonGames
+    const { data: { data: seasonGames } } = response
+    // extracts the last 10 recent games of the selected team
+    const teamRecentGames = seasonGames.slice(-10).reverse()
+    showRecentGames(teamRecentGames)
+
+  } catch (error) {
+    console.log(error)
+    errorMessage("Unavailable Games for the Selected Team")
+  }
+
+}
+
+// id would come from user input from recent games dropdown menu
+async function getTopPerformers(id) {
+  try {
+    const teamsRequest = axios.get(`https://www.balldontlie.io/api/v1/games/${id}`)
+    const statsRequest = axios.get(`https://www.balldontlie.io/api/v1/stats?game_ids[]=${id}&per_page=50`)
+    const response = await Promise.all([teamsRequest, statsRequest])
+    const [teamsResponse, statsResponse] = response
+
+    const homeTeamId = teamsResponse.data.home_team.id
+
+    // unpacks statsResponse Array data to variable gameStats
+    const { data: { data: gameStats } } = statsResponse
+
+    // assigns players to associated teams
+    const homeTeamPlayers = []
+    const awayTeamPlayers = []
+    for (let player of gameStats) {
+      player.team.id === homeTeamId ? homeTeamPlayers.push(player) : awayTeamPlayers.push(player)
+    }
+    clearqDiv()
+    showTopPerformers(homeTeamPlayers)
+    showTopPerformers(awayTeamPlayers)
+  } catch (error) {
+    console.log(error)
+    errorMessage("Unavailable Information for the Selected Game")
+  }
+
+}
+
+// PROTO EXTENSIONS
 String.prototype.capitalize = function () {
   return this.charAt(0).toUpperCase() + this.slice(1)
 }
+
